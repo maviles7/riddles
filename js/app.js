e90cbfddabe286f562c0f0ae2ff2ae0c437857cc
maviles <maviles221@gmail.com>
2024-08-02 14:48:35 -0500

    rendering riddle & answer

--- js/app.js
+++ js/app.js
@@ -1,44 +1,63 @@
 /*-------------- Constants -------------*/
 
+//object w/property of ra1 -> ra1 also has a property of riddle 
 const RIDDLESANDANSWERS = {
-    'Riddle 1: I am an odd number. Take away a letter and I become even. What number am I?': 'seven',
-    'Riddle 2: I am a word of three letters. Add two more and the less of me there will be. What word am I?': 'few',
-    'Riddle 3: The more of me there is, the less you can see. What am I?' : 'darkness',    
+    ra1: {riddle: 'I am an odd number. Take away a letter and I become even. What number am I?', answer: 'seven'}, 
+    ra2: {riddle: 'I am a word of three letters. Add two more and the less of me there will be. What word am I?', answer: 'few'}, 
+    ra3: {riddle: 'The more of me there is, the less you can see. What am I?', answer: 'darkness'}   
 }; 
+
+//creating an array from RIDDLESANDANSWERS to track riddles via index 
+const RIDDLE_KEYS = Object.keys(RIDDLESANDANSWERS); 
+//console.log(RIDDLE_KEYS); 
 
 /*---------- Variables (state) ---------*/
 
-let answer; //storing players answer 
+let currentRiddleIdx; //tracking current riddle index in RIDDLE_KEYS 
+let RiddleAns; //tracking answer to current riddle 
 let score; //checking to see if players score is correct & track 
 let winner; //did player get 2/3 answers correctly 
+let playerInput; 
 
 /*----- Cached Element References - things that need to reference the DOM elements -----*/
 
-const riddle1 = document.getElementById('r1'); 
-const riddle2 = document.getElementById('r2'); 
-const riddle3 = document.getElementById('r3'); 
+const riddle = document.querySelector('.riddle'); // accessing riddle in HTML
+const playerAnswer = document.getElementById('answer'); // accessing answer inputed into textbox from player
 
-const answer1 = document.getElementById('a1')
-const answer2 = document.getElementById('a2')
-const answer3 = document.getElementById('a3')
 
 const submitBtn = document.getElementById('submit'); 
 const resetBtn = document.getElementById('reset'); 
 
 /*----------- Event Listeners ----------*/
 
+
 /*-------------- Functions -------------*/
 init(); 
 
 function init() {
-
-}; 
-
-
-
-function render() {
-
+    currentRiddleIdx = 0;  
+    score = 0; 
+    winner = false; 
+    render(); 
 }; 
 
 // function to render riddles on the page 
-// function to submit answers
+function renderRiddles() {
+    currentRiddle = RIDDLESANDANSWERS[RIDDLE_KEYS[currentRiddleIdx]].riddle; 
+    console.log(currentRiddleIdx); 
+    riddle.innerHTML = currentRiddle; 
+}
+
+// function to render score 
+function renderScore() {
+    console.log(currentRiddle)
+    RiddleAns = RIDDLESANDANSWERS[RIDDLE_KEYS[currentRiddleIdx]].answer; 
+    console.log(RiddleAns); 
+    // if statement to compare playerInput to currentRiddleAns
+}
+
+function render() {
+    renderRiddles(); 
+    renderScore(); 
+}; 
+ 
