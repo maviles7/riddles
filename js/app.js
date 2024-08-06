5823baa907b7b5fffacbc447b2ecca71aa02bb1c
maviles <maviles221@gmail.com>
2024-08-06 10:29:10 -0500

    toLowerCase() ans

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 /*-------------- Constants -------------*/
 
 //object w/property of ra1 -> ra1 also has a property of riddle 
-const RIDDLESANDANSWERS = {
+const LEVELONE = {
     ra1: {riddle: 'I am an odd number. Take away a letter and I become even. What number am I?', answer: 'seven'}, 
     ra2: {riddle: 'I am a word of three letters. Add two more and the less of me there is. What word am I?', answer: 'few'}, 
     ra3: {riddle: 'The more of me there is, the less you can see. What am I?', answer: 'darkness'}   
 }; 
 
-//creating an array from RIDDLESANDANSWERS to track riddles via index 
-const RIDDLE_KEYS = Object.keys(RIDDLESANDANSWERS); 
-//console.log(RIDDLE_KEYS); 
+//creating an array from LEVELONE to track riddles via index 
+const LEVELONE_KEYS = Object.keys(LEVELONE); 
 
 /*---------- Variables (state) ---------*/
 
@@ -17,7 +16,7 @@ let currentRiddleIdx; //tracking current riddle index in RIDDLE_KEYS
 let riddleAns; //tracking answer to current riddle 
 let score; //checking to see if players score is correct & track 
 let winner; //did player get 2/3 answers correctly 
-let playerAns; //player's answer
+let playerAns; //player's answer 
 
 /*----- Cached Element References - things that need to reference the DOM elements -----*/
 
@@ -51,8 +50,8 @@ function init() {
 }; 
 
 function handleSubmit(event) { 
-    playerAns = playerInput.value; 
-    //console.log(playerAns)  
+    playerAns = playerInput.value.toLowerCase(); 
+    console.log(playerAns); 
     checkAnswers(); 
     checkWinner(); 
     // add 1 to currentRiddleIdx to move onto next question: 
@@ -63,19 +62,17 @@ function handleSubmit(event) {
 
 function checkAnswers() {
     // adding +1 to score if answer if correct 
-    riddleAns = RIDDLESANDANSWERS[RIDDLE_KEYS[currentRiddleIdx]].answer;
+    riddleAns = LEVELONE[LEVELONE_KEYS[currentRiddleIdx]].answer;
     if (playerAns === riddleAns) {
         score += 1; 
-        //console.log(score); 
     }; ; 
 }
 
 function checkWinner() {
-    //console.log(currentRiddleIdx + 1, RIDDLE_KEYS.length, currentRiddleIdx + 1 < RIDDLE_KEYS.length)
-    if (currentRiddleIdx + 1 < RIDDLE_KEYS.length) {
+    if (currentRiddleIdx + 1 < LEVELONE_KEYS.length) {
         return; 
     }
-    if (score > 2) { 
+    if (score >= 2) { 
         winner = true;  
         eogMessage.style.visibility = 'visible'; 
         eogMessage.innerText = "winner winner"; 
@@ -87,18 +84,19 @@ function checkWinner() {
 
 // function to render riddles on the page 
 function renderRiddles() {
-    if (currentRiddleIdx >= RIDDLE_KEYS.length) {
+    if (currentRiddleIdx >= LEVELONE_KEYS.length) {
         //making submit button & textbox disappear
         playerInput.style.visibility = 'hidden'; 
         submitBtn.style.visibility = 'hidden'; 
         riddle.style.visibility = 'hidden'; 
         return; 
     }
-    currentRiddle = RIDDLESANDANSWERS[RIDDLE_KEYS[currentRiddleIdx]].riddle; 
-    //console.log(currentRiddleIdx); 
+    currentRiddle = LEVELONE[LEVELONE_KEYS[currentRiddleIdx]].riddle; 
     riddle.innerHTML = currentRiddle; 
 }
 
 function render() {
     renderRiddles(); 
 }; 
+
+// why does reset game not clear the TB? 
